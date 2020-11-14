5edaaf6f7bc8e7ec3b9f7d5d181bb258bc045e76
kekgle <fdg@vui.com>
2020-11-14 11:58:48 +0100

    fixing a bug in download script

--- genia/utils/download.py
+++ genia/utils/download.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     bio_bert = ("1GJpGjQj6aZPV-EfbiQELpBkvlGtoKiyA", 'biobert_large_v1.1_pubmed.tar.gz')
     bio_nlp_vec = ("0BzMCqpcgEJgiUWs0ZnU0NlFTam8", 'bio_nlp_vec.tar.gz')
 
-    for file_name, file_id in [bio_nlp_vec, bio_bert]:
+    for file_id, file_name in [bio_nlp_vec, bio_bert]:
         print(f'downloading {file_name}...')
         downloaded = drive.CreateFile({'id': file_id})
         downloaded.GetContentFile(file_name)
